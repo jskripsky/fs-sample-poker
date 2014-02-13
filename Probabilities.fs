f3d54ca5764fa6a5021c3a30c20276d0c909aa14
Juraj Skripsky <js@hotfeet.ch>
2014-02-14 00:00:56 +0100

    Add usage examples as comments.

--- Probabilities.fs
+++ Probabilities.fs
@@ -2,10 +2,21 @@ module Poker.Probabilities
 
 let map f dist = dist |> Seq.map (fun o -> { Value = f o.Value; Probability = o.Probability })
  
+// selectOne [1; 2; 3] =
+//	seq [ { Value = (1, [2; 3]); Probability = 1.0 / 3.0 }
+//	 	{ Value = (2, [1; 3]); Probability = 1.0 / 3.0 }
+//	 	{ Value = (3, [1; 2]); Probability = 1.0 / 3.0 } ]
 let selectOne values =
 	[for e in values -> e, values |> Seq.filter ((<>) e)]
 	|> toUniformDistribution
  
+// selectMany 0 [1; 2; 3] = seq [{Value = ([], [1; 2; 3]); Probability = 1.0;}]
+// selectMany 1 [1; 2; 3] = selectOne [1; 2; 3]
+// selectMany 2 [1; 2; 3] =
+//	seq [ { Value = ([1; 2], seq [3]); Probability = 1.0 / 6.0 }
+//		 { Value = ([1; 3], seq [2]); Probability = 1.0 / 6.0 } 
+//		 [... 3 ...]
+//		 { Value = ([3; 2], seq [1]); Probability = 1.0 / 6.0 } ]
 let rec selectMany n values =
 	match n with
 	| 0 -> certainly ([], values)
@@ -16,6 +27,11 @@ let rec selectMany n values =
 			return x::xs,c2 }
 
 let select n values = selectMany n values |> map (fst >> List.rev)
+// select 2 [1; 2; 3] =
+//	seq [ { Value = [2; 1]; Probability = 1.0 / 6.0 }
+//		{ Value = [3; 1]; Probability = 1.0 / 6.0 }
+//		[... 3 ...]
+//		{ Value = [3; 2]; Probability = 1.0 / 6.0 } ]
 let remove items = Seq.filter (fun v -> Seq.forall ((<>) v) items)
 
 let filterInAnyOrder items dist =
